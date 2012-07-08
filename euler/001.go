d1202a6b659fa653099d848bb08a34469457ae4f
Joe Carnahan <joseph.carnahan@gmail.com>
2012-07-08 16:34:53 -0400

    Switched to slightly more efficient implementation for solution 1

--- euler/001.go
+++ euler/001.go
@@ -4,14 +4,23 @@ const Problem001Default = 1000
 
 func Problem001(limit int) int {
 	multiples := []int{3, 5}
+	product := multiples[0] * multiples[1]
 	sum := 0
-	for i := 0; i < limit; i++ {
-		for _, multiple := range multiples {
-			if i % multiple == 0 {
-				sum += i
-				break
-			}
+
+	// Sum up all multiples of 3 and multiples of 5
+	for _, multiple := range multiples {
+		current := multiple
+		for current < limit {
+			sum += current
+			current += multiple
 		}
 	}
+	// Then subtract all multiples of 15, as they were double-counted
+	current := product
+	for current < limit {
+		sum -= current
+		current += product
+	}
+
 	return sum
 }
