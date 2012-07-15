aee2ac1826605434b9517f0a76a31cac503b5fe1
Joe Carnahan <joseph.carnahan@gmail.com>
2012-07-14 22:47:23 -0400

    Added int64 version of the running-and-timing function

--- runner/runner.go
+++ runner/runner.go
@@ -14,3 +14,13 @@ func RunInt(f func(int) int, arg int, reps int) {
 	end := time.Now()
 	fmt.Printf("%v\nRunning %v times took %v.\n", result, reps, end.Sub(start))
 }
+
+func RunInt64(f func(int64) int64, arg int64, reps int) {
+	var result int64
+	start := time.Now()
+	for i := 0; i < reps; i++ {
+		result = f(arg)
+	}
+	end := time.Now()
+	fmt.Printf("%v\nRunning %v times took %v.\n", result, reps, end.Sub(start))
+}
