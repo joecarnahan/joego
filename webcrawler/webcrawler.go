08e13c6cef7432d1d8c675dd2abb78d11a07197d
Joe Carnahan <joseph.carnahan@gmail.com>
2013-07-14 17:37:28 -0400

    Working on parallel crawler solution, not much progress yet

--- webcrawler/webcrawler.go
+++ webcrawler/webcrawler.go
@@ -1,12 +1,77 @@
 package webcrawler
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
 	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
+
+type result struct {
+	url string
+	depth int
+}
+
+// Reads results from allResults and writes out any results that should be crawled based
+// on what results have been seen so far. Specifically, the same URL will only be crawled
+// twice if it is encountered again at a higher depth than it was encountered before.
+//
+// Loops until the allResults channel is closed. Closes the filteredResults channel before
+// exiting.
+func filterResults(allResults chan result, filteredResults chan result) {
+	urlDepths := make(map[string]int)
+	for result := range allResults {
+		if (urlDepths[result.url] == 0) || /* RESUME HERE Figure out depth calculation */ (urlDepths[result.url] {
+			urlDepths[result.url] = result.depth
+			filteredResults <- result
+		}
+	}
+}
+
+// Prints any strings that are sent to it to the given Writer. Loops until the given
+// channel is closed.
+func printStrings(toPrint chan string, output io.Writer) {
+	for s := range toPrint {
+		io.WriteString(output, s)
+	}
+}
+
+func parallelCrawl(url string, depth int, 
+
+func Crawl(url string, depth int, fetcher Fetcher) {
+	output := make(chan string)
+	go printStrings(output, os.Stdout)
+	allUrls := make(chan string)
+	filteredUrls := make(chan string)
+	go filterUrls(allUrls, filteredUrls)
+	// Set up URL filter
+	// Set up printer
+	// Set up crawler registry
+	// Send first URL to URL filter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Receives URL strings on the input channel, sends unique URL strings
 // on the output channel. In other words, each URL might be given to
@@ -56,11 +121,11 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	}
 	register := make(chan bool)
 	unregister := make(chan bool)
-	uniqueUrls := make(chan string)
+	filteredUrls := make(chan string)
 	allUrls := make(chan string)
 	go WriteUrl(allUrls, url)
-	go ManageUrls(allUrls, uniqueUrls, register, unregister)
-	for uniqueUrl := range uniqueUrls {
+	go ManageUrls(allUrls, filteredUrls, register, unregister)
+	for uniqueUrl := range filteredUrls {
 		go ParallelCrawl(uniqueUrl, allUrls, depth-1, fetcher)
 	}
 
