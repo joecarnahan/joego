a2e1980951f54a2a0e6b27e0a9b9e9e57afe6ff8
Joe Carnahan <joseph.carnahan@gmail.com>
2013-07-21 08:03:56 -0400

    Started working on a new solution strategy for problem 5

--- euler/005.go
+++ euler/005.go
@@ -2,9 +2,12 @@ package euler
 
 import "container/list"
 
-const Problem005Default = 20
+const Problem005Default = 10
 
 func Problem005(limit int64) int64 {
+	// Map from factors to the number of times they are a factor
+	// TODO RESUME HERE
+	factors := make(map[int64]int64)
 	factors := list.New()
 	for i := int64(2); i <= limit; i++ {
 		newFactors := PrimeFactorsOf(i)
