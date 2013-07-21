ea3edd420a360dca1a11b2e2e4dda9855c91256f
Joe Carnahan <joseph.carnahan@gmail.com>
2013-07-20 21:37:45 -0400

    Added unit test for RemoveIfPresent

--- euler/lists.go
+++ euler/lists.go
@@ -3,7 +3,7 @@ package euler
 import "container/list"
 
 // Removes the first occurrence of toRemove from the given list.
-func RemoveIfPresent(fromList *list.List, toRemove interface{}) {
+func removeIfPresent(fromList *list.List, toRemove interface{}) {
 	for e := fromList.Front(); e != nil; e = e.Next() {
 		if e.Value == toRemove {
 			fromList.Remove(e)
@@ -28,7 +28,7 @@ func AddAll(list1, list2 *list.List) {
 func MergeLists(list1, list2 *list.List) *list.List {
 	result := list.New()
 	for e := list1.Front(); e != nil; e = e.Next() {
-		RemoveIfPresent(list2, e.Value)
+		removeIfPresent(list2, e.Value)
 		result.PushBack(e.Value)
 	}
 	AddAll(result, list2)
--- euler/lists_test.go
+++ euler/lists_test.go
@@ -1,13 +1,52 @@
 package euler
 
 import (
-	// TODO Implement when used
-	// "container/list"
+	"container/list"
 	"testing"
 )
 
-func TestRemoveIfPresent(t *testing.T) {
-	// TODO Implement this
+func TestremoveIfPresent(t *testing.T) {
+	myList := list.New()
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 0 {
+		t.Errorf("Expected an empty list, got a list of length %v", length)
+	}
+	myList.PushBack(3)
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 0 {
+		t.Errorf("Expected an empty list, got a list of length %v", length)
+	}
+	myList.PushBack(2)
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 1 {
+		t.Errorf("Expected a list of length 1, got a list of length %v", length)
+	} else if headElement := myList.Front(); headElement.Value != 2 {
+		t.Errorf("Expected an element 2, got %v", headElement.Value)
+	}
+	myList.PushBack(3)
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 1 {
+		t.Errorf("Expected a list of length 1, got a list of length %v", length)
+	} else if headElement := myList.Front(); headElement.Value != 2 {
+		t.Errorf("Expected an element 2, got %v", headElement.Value)
+	}
+	myList.PushFront(3)
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 1 {
+		t.Errorf("Expected a list of length 1, got a list of length %v", length)
+	} else if headElement := myList.Front(); headElement.Value != 2 {
+		t.Errorf("Expected an element 2, got %v", headElement.Value)
+	}
+	myList.PushBack(3)
+	myList.PushBack(1)
+	removeIfPresent(myList, 3)
+	if length := myList.Len(); length != 2 {
+		t.Errorf("Expected a list of length 2, got a list of length %v", length)
+	} else if headElement := myList.Front(); headElement.Value != 2 {
+		t.Errorf("Expected a head element 2, got %v", headElement.Value)
+	} else if tailElement := myList.Back(); tailElement.Value != 1 {
+		t.Errorf("Expected a tailelement 1, got %v", tailElement.Value)
+	}
 }
 
 func TestAddAll(t *testing.T) {
