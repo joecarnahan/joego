bf9b4b46153a9fc525f58c8e826b5b2a007f5983
Joe Carnahan <joseph.carnahan@gmail.com>
2013-07-21 20:07:33 -0400

    Added test for mergeMaps

--- euler/005_test.go
+++ euler/005_test.go
@@ -3,7 +3,63 @@ package euler
 import "testing"
 
 func TestMergeMaps(t *testing.T) {
-	// TODO Implement this
+	empty := make(map[int64]int64)
+	empty2 := make(map[int64]int64)
+	mergeMaps(empty, empty2)
+	if len(empty) != 0 {
+		t.Errorf("Expected result to be empty, got %v", empty)
+	}
+	has11 := make(map[int64]int64)
+	has11[int64(1)] = int64(1)
+	mergeMaps(has11, empty)
+	if len(has11) != 1 {
+		t.Errorf("Expected result to have one element, got %v", has11)
+	} else if has11[int64(1)] != int64(1) {
+		t.Errorf("Expected result[1] to be 1, got %v", has11[int64(1)])
+	}
+	emptyGets11 := make(map[int64]int64)
+	mergeMaps(emptyGets11, has11)
+	if len(has11) != 1 {
+		t.Errorf("Expected has11 to still have one element, got %v", has11)
+	} else if has11[int64(1)] != int64(1) {
+		t.Errorf("Expected has11[1] to be 1, got %v", has11[int64(1)])
+	}
+	if len(emptyGets11) != 1 {
+		t.Errorf("Expected result to have one element, got %v", emptyGets11)
+	} else if emptyGets11[int64(1)] != int64(1) {
+		t.Errorf("Expected result[1] to be 1, got %v", emptyGets11[int64(1)])
+	}
+	has11Gets11 := make(map[int64]int64)
+	has11Gets11[int64(1)] = int64(1)
+	mergeMaps(has11Gets11, has11)
+	if len(has11) != 1 {
+		t.Errorf("Expected has11 to still have one element, got %v", has11)
+	} else if has11[int64(1)] != int64(1) {
+		t.Errorf("Expected has11[1] to be 1, got %v", has11[int64(1)])
+	}
+	if len(has11Gets11) != 1 {
+		t.Errorf("Expected result to have one element, got %v", has11Gets11)
+	} else if has11Gets11[int64(1)] != int64(2) {
+		t.Errorf("Expected result[1] to be 2, got %v", has11Gets11[int64(1)])
+	}
+	has2345Gets11 := make(map[int64]int64)
+	has2345Gets11[int64(2)] = int64(3)
+	has2345Gets11[int64(4)] = int64(5)
+	mergeMaps(has2345Gets11, has11)
+	if len(has11) != 1 {
+		t.Errorf("Expected has11 to still have one element, got %v", has11)
+	} else if has11[int64(1)] != int64(1) {
+		t.Errorf("Expected has11[1] to be 1, got %v", has11[int64(1)])
+	}
+	if len(has2345Gets11) != 3 {
+		t.Errorf("Expected result to have three elements, got %v", has2345Gets11)
+	} else if has2345Gets11[int64(1)] != int64(1) {
+		t.Errorf("Expected result[1] to be 1, got %v", has2345Gets11[int64(1)])
+	} else if has2345Gets11[int64(2)] != int64(3) {
+		t.Errorf("Expected result[2] to be 3, got %v", has2345Gets11[int64(2)])
+	} else if has2345Gets11[int64(4)] != int64(5) {
+		t.Errorf("Expected result[4] to be 5, got %v", has2345Gets11[int64(4)])
+	}
 }
 
 // TODO Figure out why this test doesn't pass
